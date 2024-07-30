bdee87199c40d0b2c10dc48247a68867bbbe8da9
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-30 21:24:20 +0300

    Update kelvin.js

--- kelvin.js
+++ kelvin.js
@@ -1,5 +1,5 @@
-const kelvin = 293; //created a constant for kelvin
-let celsius = 293 - 273;//convert Kelvin to Celsius by subtracting 273 from the kelvin variable. 
+const kelvin = 0; //created a constant for kelvin
+let celsius = kelvin - 273;//convert Kelvin to Celsius by subtracting 273 from the kelvin variable. 
 let fahrenheit = celsius * (9/5) + 32;//calculated ahrenheit, then stored the answer in a variable named fahrenheit.
 fahrenheit = Math.floor(fahrenheit);//Used the .floor() method from the built-in Math object to round down the Fahrenheit temperature.
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
