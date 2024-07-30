a4f4a25a408febc318c2211f763ce39640e8abe9
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-30 18:55:01 +0300

    string interpolation.

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ console.log(lostDollar);
 
 const favoriteAnimal = 'Eagle';
 console.log('My favorite animal: ' + favoriteAnimal)// String Concatenation with Variables
+
+let myName = 'Austin';
+let favoriteCity = 'Nairobi';
+console.log(`My name is ${myName}, and my favorite city is ${favoriteCity}`)//using string interpolation
