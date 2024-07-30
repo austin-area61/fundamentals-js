1f9d594a42e3698b1081de760a250a85eb6db274
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-30 21:31:16 +0300

    Update kelvin.js. Added alternative project for turning it into degree newton.

--- kelvin.js
+++ kelvin.js
@@ -3,3 +3,11 @@ let celsius = kelvin - 273;//convert Kelvin to Celsius by subtracting 273 from t
 let fahrenheit = celsius * (9/5) + 32;//calculated ahrenheit, then stored the answer in a variable named fahrenheit.
 fahrenheit = Math.floor(fahrenheit);//Used the .floor() method from the built-in Math object to round down the Fahrenheit temperature.
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+/*
+const newton = 0; //created a constant for kelvin
+let celsius = newton - 273;//convert Kelvin to Celsius by subtracting 273 from the kelvin variable. 
+let fahrenheit = celsius * (33/100);//calculated ahrenheit, then stored the answer in a variable named fahrenheit.
+fahrenheit = Math.floor(fahrenheit);//Used the .floor() method from the built-in Math object to round down the Fahrenheit temperature.
+console.log(`The temperature is ${fahrenheit} degrees newton.`)
+*/
