5ed4b9f1d9e25b4cd1a80ddc92a6dee023e96a7c
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-30 21:50:57 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const myAge = 24;//Create a variable named myAge, and set it equal to my age
+let earlyYears = 2;//
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2;
+laterYears += 4;//the number of dog years accounted for by my later years
+console.log(earlyYears)
+console.log(laterYears)
+let myAgeInDogYears = earlyYears + laterYears;//my age in dog years
+let myName = 'Austin'.toLowerCase();//stored my name as a variable and used a built in method to return the string to lower case letters.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);//Used string interpolation to display the value
