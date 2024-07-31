538619275b503b8fb844b86279a0dade5b903c85
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-31 10:32:34 +0300

    Update conditionals.js by using truthy and falsy statements. Learning how these specific statements affect the execution of if and else statements.

--- conditionals.js
+++ conditionals.js
@@ -6,3 +6,21 @@ if (mood === 'sleepy' && tirednessLevel > 8){
 } else {
   console.log('not bed time yet');
 }
+
+//truthy and falsy statements
+let wordCount = 14;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
