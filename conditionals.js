3c1922c30fea7dd7d9a7c3b6c39cc796a59edcf1
Austin Axwell <austinonyango.area61@gmail.com>
2024-07-31 11:05:56 +0300

    Update conditionals.js by using ternary operators in place of if and else statements.

--- conditionals.js
+++ conditionals.js
@@ -34,5 +34,35 @@ if({
   
 }
 */
-
 console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+//ternary operator
+let isLocked = false;
+/*
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
+*/
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+/*
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+*/
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+favoritePhrase ==='Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+/*
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+*/
