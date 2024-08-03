6271adc95b301d81598107731039e7e1c4e7826c
Austin Axwell <austinonyango.area61@gmail.com>
2024-08-03 19:23:41 +0300

    default parameters

--- func1.js
+++ func1.js
@@ -11,3 +11,11 @@ function sayThanks(name) {
   console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
 }
 sayThanks('Cole');
+
+//default parameters i.e When the code calls greeting('Austin') the value of the argument is passed in and, 'Austin', will override the default parameter of 'stranger' to log 'Hello, Austin!' to the console.
+function greeting (name = 'stranger') {
+  console.log(`Hello, ${name}!`)
+}
+
+greeting('Austin') // Output: Hello, Austin!
+greeting() // Output: Hello, stranger!
