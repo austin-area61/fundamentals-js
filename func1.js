57958dc82ed70960629b9d9e5758f2e0c2aaf478
Austin Axwell <austinonyango.area61@gmail.com>
2024-08-03 19:17:52 +0300

    Update func1.js by assigning arguments inside parameters in functions

--- func1.js
+++ func1.js
@@ -5,3 +5,9 @@ function sayThanks(){
 sayThanks();
 sayThanks();
 sayThanks();
+
+//assigning arguments inside parameters in functions
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+}
+sayThanks('Cole');
