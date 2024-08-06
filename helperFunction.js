6cc6ebca03b83e55fa47725ad3af88b701c5145c
Austin Axwell <austinonyango.area61@gmail.com>
2024-08-06 22:07:11 +0300

    Update helperFunction.js

--- helperFunction.js
+++ helperFunction.js
@@ -10,3 +10,15 @@ console.log(totalCost);
 totalCost = 16;
 console.log(totalCost);
 */
+
+
+//defining a function using a function expression
+const plantNeedsWater = function (day) {
+  if( day === 'Wednesday'){
+    return true;
+  } else {
+    return false;
+  }
+}
+plantNeedsWater('Tuesday');
+console.log(plantNeedsWater('Tuesday')); //Should log false
