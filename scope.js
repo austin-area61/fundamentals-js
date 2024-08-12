626427b8eec8f4400fa20865cfe1740ba1f7ee22
Austin Axwell <austinonyango.area61@gmail.com>
2024-08-12 19:01:21 +0300

    Added some comments

--- scope.js
+++ scope.js
@@ -15,3 +15,10 @@ const logVisibleLightWaves = () => {
 }
 logVisibleLightWaves();
 console.log(lightWaves);
+/*
+Tightly scoping your variables will greatly improve code in several ways:
+It will make your code more legible since the blocks will organize your code into discrete sections.
+It makes your code more understandable since it clarifies which variables are associated with different parts of the program rather than having to keep track of them line after line!
+It’s easier to maintain your code, since your code will be modular.
+It will save memory in your code because it will cease to exist after the block finishes running.
+*/
