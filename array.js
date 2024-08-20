d4d1ba27b62ac7ad4712d627b2152ba7db7b2cb7
Austin Axwell <austinonyango.area61@gmail.com>
2024-08-20 07:45:06 +0300

    Update array.js

--- array.js
+++ array.js
@@ -7,3 +7,24 @@ console.log(famousSayings[3]);//prints undefined in the console
 //updating elements inside arrays
 let groceryList = ['bread', 'tomatoes', 'milk'];
 groceryList[1] = 'avocados';
+
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);// you can re-assign elements in an array and re-assign an entire new array to a variable declared using the let keyword.
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+newGroceryList = groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
