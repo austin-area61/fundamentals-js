456abc60210f7aa73224cf4c3cdd19ba94df4fe0
Austin Axwell <austinonyango.area61@gmail.com>
2024-10-02 16:14:00 +0300

    Update loops.js

--- loops.js
+++ loops.js
@@ -2,3 +2,11 @@ const vacationSpots = ['Spain','Japan','North Pole'];
 console.log(vacationSpots[0])
 console.log(vacationSpots[1])
 console.log(vacationSpots[2])
+
+for (let counter = 5; counter <= 10; counter++) {
+  console.log(counter);
+}
+//in reverse
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
